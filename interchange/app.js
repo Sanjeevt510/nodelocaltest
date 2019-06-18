94b90070cdd46704389d1a780ffb7970fee02cf4
lenovo-dev\dev <sanjeev@tiggacom.com>
2019-06-18 17:54:05 +1000

    adding outbound routee

--- interchange/app.js
+++ interchange/app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+let  esclationRouter = require('./routes/api/escalation');
+let  callBack = require('./routes/api/callback');
+
+
 var app = express();
 
 // view engine setup
@@ -21,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/esc', esclationRouter);
+app.use('/cc', callBack);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
